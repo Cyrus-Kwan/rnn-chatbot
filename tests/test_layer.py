072739b5abe4761417958872ad9855b78c2b5c90
MQCyrusKwan <cyrus.kwan@outlook.com>
2025-08-19 13:06:04 +1000

    initialize layers and weights

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -18,7 +18,35 @@ class TestLayer(unittest.TestCase):
         cur_path:Path   = Path(__file__).parent.resolve()
         with open(cur_path / "test_layer.json") as f:
             self.test_cases:dict    = json.load(f)
+            self.active:dict        = {
+                "sign_zero":Activation.sign_zero,
+                "sign_one":Activation.sign_one,
+                "logistic":Activation.logistic,
+                "bipolar":Activation.bipolar,
+                "hyper_log":Activation.hyper_log
+            }
+            self.learn:dict         = {
+                "hebbian":LearningRule.hebbian,
+                "discrete":LearningRule.discrete,
+                "gradient":LearningRule.gradient
+            }
         return super().setUp()
     
     def test_forward(self):
+        '''
+        Computes the outputs/activation of each neuron in the layer
+        '''
+        for case in self.test_cases["test_forward"]:
+            layer   = Layer(
+                num_neurons = case["num_neurons"],
+                num_inputs  = case["num_inputs"],
+                active      = self.active[case["active"]],
+                learn       = self.learn[case["learn"]]
+            )
+
+            for n, neuron in enumerate(case["neurons"]):
+                # Initial weights for each neuron
+                layer.neurons[n].weights    = neuron["weights"]
+                
+                # TODO: forward pass for each input vector.
         return
