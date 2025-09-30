4dcaecfcb181ace8765887c62918beb228da59f9
MQCyrusKwan <cyrus.kwan@outlook.com>
2025-09-30 13:45:29 +1000

    Added derivative variable and modified neuron input update

--- src/layer.py
+++ src/layer.py
@@ -3,15 +3,22 @@ from neuron import *
 
 class Layer():
     def __init__(self, num_neurons: int, num_inputs: int, active: Callable, learn: Callable):
-        self.outputs    = [0.0*num_neurons]
+        self.outputs    = [0.0] * num_neurons
         self.num_inputs = num_inputs
         self.learn      = learn
+        self.derivative = None
+
+        # Set the derivative function for backpropagation
+        if active == Activation.bipolar:
+            self.derivative = Derivative.bipolar
+        elif active == Activation.logistic:
+            self.derivative = Derivative.logistic
         
         # Initialize an array of zero'd neurons equal to the params
         self.neurons    = [
             Neuron(
-                inputs  = [0.0*num_inputs],
-                weights = [0.0*num_inputs],
+                inputs  = [0.0] * num_inputs + [1], # Include bias term input
+                weights = [0.0] * num_inputs + [1], # Include bias term weight
                 active  = active,
                 learn   = learn
             ) for n in range(num_neurons)
@@ -23,7 +30,8 @@ class Layer():
         '''
         outputs = []
         for neuron in self.neurons:
-            neuron.inputs   = inputs
+            for v, vector in enumerate(inputs):
+                neuron.inputs[v] = vector
             outputs.append(neuron.active(neuron))
 
         self.outputs    = outputs
