17e29d14a8ae026e65384993528b6e9768eff297
Cyrus_Kwan <cyrus.kwan@outlook.com>
2025-10-07 19:40:01 +1100

    relu and softmax derivatives added

--- src/layer.py
+++ src/layer.py
@@ -6,24 +6,33 @@ class Layer():
     def __init__(self, num_neurons:int, num_inputs:int, active:Callable, learn:Callable, random_seed:int):
         random.seed(random_seed)
 
-        self.outputs    = [0.0] * num_neurons
+        self.outputs    = [0.0 for _ in range(num_neurons)]
         self.num_inputs = num_inputs
         self.learn      = learn
+        self.active     = active
         self.derivative = None
 
         # Set the derivative function for backpropagation
-        if active == Activation.bipolar:
+        if active == Activation.sign_zero:
+            self.derivative = Derivative.sign_zero
+        elif active == Activation.sign_one:
+            self.derivative = Derivative.sign_one
+        elif active == Activation.bipolar:
             self.derivative = Derivative.bipolar
         elif active == Activation.logistic:
             self.derivative = Derivative.logistic
+        elif active == Activation.hyper_log:
+            self.derivative = Derivative.hyper_log
         elif active == Activation.relu:
             self.derivative = Derivative.relu
+        elif active == Activation.softmax:
+            self.derivative = Derivative.softmax
         
         # Initialize an array of zero'd neurons equal to the params
         self.neurons    = [
             Neuron(
-                inputs  = [random.uniform(-0.5, 0.5)] * num_inputs + [1], # Include bias term input
-                weights = [random.uniform(-0.5, 0.5)] * num_inputs + [1], # Include bias term weight
+                inputs  = [random.uniform(-0.5, 0.5) for _ in range(num_inputs)] + [1], # Include bias term input
+                weights = [random.uniform(-0.5, 0.5) for _ in range(num_inputs)] + [1], # Include bias term weight
                 active  = active,
                 learn   = learn
             ) for n in range(num_neurons)
@@ -36,13 +45,10 @@ class Layer():
         outputs = []
         for neuron in self.neurons:
             neuron.inputs[:-1]  = inputs    # Do not overwrite bias
-            outputs.append(neuron.active(neuron))
+            if self.active == Activation.softmax:
+                outputs.append(neuron.active(self.neurons))
+            else:
+                outputs.append(neuron.active())
 
         self.outputs    = outputs
         return outputs
-
-def main():
-    return
-
-if __name__ == "__main__":
-    main()
